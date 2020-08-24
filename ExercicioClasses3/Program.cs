ab9d8c6911c6766a01b0db3644a10445c8eaa769
Andreia de Moraes <andreiadmrs@gmail.com>
2020-08-23 22:52:32 -0300

    Exercício do curso de C#. Classes, atributos e métodos.

---
+++ ExercicioClasses3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioClasses3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Fazer um programa para ler os valores da largura e altura
+            de um retângulo. Em seguida, mostrar na tela o valor de
+            sua área, perímetro e diagonal. Usar uma classe como
+            mostrado no projeto ao lado.*/
+            var retangulo = new Retangulo();
+
+            Console.WriteLine("Digite largura do retangulo:");
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite altura do retangulo:");
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Area: "+retangulo.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perímetro: "+ retangulo.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal: " + retangulo.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
